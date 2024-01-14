6b2779675d02de37127afd76f328bfe462d9fba1
Bernie Sumption <bernie@berniecode.com>
2024-01-14 12:11:26 +0000

    WIP

--- grid-packages/ag-grid-theme-builder/src/features/editors/color/PercentColorEditor.tsx
+++ grid-packages/ag-grid-theme-builder/src/features/editors/color/PercentColorEditor.tsx
@@ -1,5 +1,5 @@
 import { InformationFilled } from '@carbon/icons-react';
-import { Slider, Stack, Tooltip } from '@mui/joy';
+import { Button, Slider, Stack, Tooltip, Typography } from '@mui/joy';
 import { useChangeHandler } from 'components/component-utils';
 import { singleOrFirst } from 'model/utils';
 import { useState } from 'react';
@@ -8,19 +8,37 @@ import {
   UncontrolledColorEditorProps,
   colorValueToCssExpression,
   formatProportionAsPercentPoint5,
+  reinterpretCssColorExpression,
 } from './color-editor-utils';
 
 export const PercentColorEditor = ({ initialValue, onChange }: UncontrolledColorEditorProps) => {
-  const [value, setValue] = useState(typeof initialValue === 'number' ? initialValue : 1);
+  const [value, setValue] = useState(initialValue);
 
   useChangeHandler(value, onChange);
+
+  if (typeof value !== 'number') {
+    return (
+      <Stack>
+        <Typography level="body-sm">{whyChoosePercent}</Typography>
+        <Button
+          onClick={() => {
+            const rgba = reinterpretCssColorExpression(value);
+            setValue(rgba?.a ?? 1);
+          }}
+          variant="soft"
+        >
+          Enable % foreground
+        </Button>
+      </Stack>
+    );
+  }
 
   return (
     <Stack>
       <ColorSwatch color={colorValueToCssExpression(value)} height={60} />
       <Stack direction="row" gap={1} alignItems="center">
         Foreground with {formatProportionAsPercentPoint5(value)} opacity
-        <Tooltip title="Specifying colours as semi-transparent versions of the foreground colour where possible ensures that the color looks good against any background, even if the background is changed later.">
+        <Tooltip title={whyChoosePercent}>
           <InformationFilled color="var(--joy-palette-primary-400)" />
         </Tooltip>
       </Stack>
@@ -38,3 +56,6 @@ export const PercentColorEditor = ({ initialValue, onChange }: UncontrolledColor
     </Stack>
   );
 };
+
+const whyChoosePercent =
+  'Specifying colours as semi-transparent versions of the foreground colour where possible ensures that the color looks good against any background, even if the background is changed later.';
