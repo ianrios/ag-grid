abcef1a6ffd74f151c7a6bfe6e7e5e6343484fb8
Bernie Sumption <bernie@berniecode.com>
2024-01-16 15:08:42 +0000

    WIP

--- grid-packages/ag-grid-theme-builder/src/features/editors/color/VarColorEditor.tsx
+++ grid-packages/ag-grid-theme-builder/src/features/editors/color/VarColorEditor.tsx
@@ -4,6 +4,8 @@ import {
   AutocompleteOption,
   Box,
   Button,
+  List,
+  ListItem,
   Slider,
   Stack,
   Tooltip,
@@ -26,7 +28,7 @@ export const VarColorEditor = ({ initialValue, onChange }: UncontrolledColorEdit
 
   useEffect(() => {
     if (editorState) {
-      setValue(new VarColor(editorState.variable, editorState.alpha).toColorValue());
+      setValue(new VarColor(editorState.variable.name, editorState.alpha).toColorValue());
     }
   }, [editorState]);
 
@@ -35,9 +37,7 @@ export const VarColorEditor = ({ initialValue, onChange }: UncontrolledColorEdit
       <Stack>
         <Typography level="body-sm">
           Specify colours as semi-transparent versions of other colours
-          <Tooltip title={whyChooseVar}>
-            <InformationIcon />
-          </Tooltip>
+          <InfoTooltip />
         </Typography>
         <Button
           onClick={() => {
@@ -45,7 +45,7 @@ export const VarColorEditor = ({ initialValue, onChange }: UncontrolledColorEdit
             const rgba = RGBAColor.reinterpretCss(value);
             setEditorState({
               alpha: rgba?.a ?? 1,
-              variable: '--ag-foreground-color',
+              variable: defaultVariable,
             });
           }}
           variant="soft"
@@ -57,7 +57,7 @@ export const VarColorEditor = ({ initialValue, onChange }: UncontrolledColorEdit
   }
 
   const feedback =
-    titleCase(formatVariable(editorState.variable)) +
+    titleCase(formatVariable(editorState.variable.name)) +
     ' with ' +
     formatProportionAs3dpPercent(editorState.alpha) +
     ' alpha';
@@ -67,27 +67,23 @@ export const VarColorEditor = ({ initialValue, onChange }: UncontrolledColorEdit
       <ColorSwatch color={value} />
       <Box>
         {feedback}
-        <Tooltip title={whyChooseVar}>
-          <InformationIcon />
-        </Tooltip>
+        <InfoTooltip />
       </Box>
       <TwoColumnTable rowSpacing={1}>
         <Cell>Color</Cell>
-        <Autocomplete
+        <Autocomplete<VariableOrHeader>
+          options={allVariables}
           value={editorState.variable}
           onChange={(_, variable) => {
             enableChangeEvents();
-            if (variable) {
+            if (variable && variable.type === 'variable') {
               setEditorState({ ...editorState, variable });
             }
           }}
           placeholder="Choose a color"
-          options={['--ag-foreground-color', '--ag-background-color', '--ag-accent-color']}
-          getOptionLabel={formatVariable}
+          getOptionLabel={({ label }) => label}
           renderOption={(props, option) => (
-            <AutocompleteOption {...props}>
-              <VariableOption key={option} variable={option} />
-            </AutocompleteOption>
+            <VariableOption autocompleteProps={props} info={option} />
           )}
           slotProps={{ listbox: { sx: { minWidth: '400px' } } }}
         />
@@ -118,35 +114,48 @@ const InformationIcon = styled(Information)`
   margin-left: 8px;
 `;
 
-const whyChooseVar =
-  'We recommend specifying related colours as semi-transparent versions of each other where possible. This makes your theme more maintainable and ensures that the color looks good against any background, even if the background is changed later. Any elements that require a neutral colour, such as borders, can be specified as a semi-transparent version of the foreground colour.';
-
 type EditorState = {
-  variable: string;
+  variable: VariableInfo;
   alpha: number;
 };
 
 const getInitialEditorState = (initialValue: string | number): EditorState | null => {
   const color = VarColor.parseCss(initialValue);
   if (!color) return null;
+  const info = allVariables.find((v) => v.type === 'variable' && v.name === color.variable);
+  if (!info || info.type != 'variable') return null;
   return {
-    variable: color.variable,
+    variable: info,
     alpha: color.alpha,
   };
 };
 
 const formatVariable = (variable: string) =>
-  titleCase(variable.replace(/^--ag-/i, '').replace(/-color$/i, ''));
+  titleCase(variable.replace(/^--ag-/i, '').replace(/-color/i, ''));
 
-const VariableOption = ({ variable }: { variable: string }) => (
-  <Stack direction="row" alignItems="center" className="ag-theme-custom">
-    <VarColorSwatch color={`var(${variable})`} />
-    <Stack gap={0}>
-      {formatVariable(variable)}
-      <VariableNameHint>var({variable})</VariableNameHint>
-    </Stack>
-  </Stack>
-);
+const VariableOption = ({
+  info,
+  autocompleteProps,
+}: {
+  info: VariableOrHeader;
+  autocompleteProps: any;
+}) => {
+  if (info.type === 'header') {
+    return <ListItem key={info.id}>{info.label}</ListItem>;
+  }
+  const variable = info.name;
+  return (
+    <AutocompleteOption key={info.id} {...autocompleteProps}>
+      <Stack direction="row" alignItems="center" className="ag-theme-custom">
+        <VarColorSwatch color={`var(${variable})`} />
+        <Stack gap={0}>
+          {formatVariable(variable)}
+          <VariableNameHint>var({variable})</VariableNameHint>
+        </Stack>
+      </Stack>
+    </AutocompleteOption>
+  );
+};
 
 const VariableNameHint = styled('span')`
   font-size: 0.8em;
@@ -158,3 +167,193 @@ const VarColorSwatch = styled(ColorSwatch)`
   height: 40px;
   border-radius: 6px;
 `;
+
+const InfoTooltip = () => (
+  <Tooltip
+    title={
+      <InfoBox padding={1}>
+        Tips:
+        <InfoList marker="disc">
+          <InfoListItem>
+            Specify related colours as semi-transparent versions of each other where possible
+          </InfoListItem>
+          <InfoListItem>
+            Use a semi-transparent version of the foreground colour for elements that require a
+            neutral colour such as borders
+          </InfoListItem>
+        </InfoList>
+        This makes your theme more maintainable and ensures that colors look good against any
+        background, even if the background is changed later.
+      </InfoBox>
+    }
+  >
+    <InformationIcon />
+  </Tooltip>
+);
+
+const InfoBox = styled(Box)`
+  color: inherit;
+  font-size: inherit;
+  margin: 0;
+  line-height: inherit;
+`;
+
+const InfoList = styled(List)`
+  color: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  margin: 0;
+`;
+
+const InfoListItem = styled(ListItem)`
+  color: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  margin: 0;
+`;
+
+type VariableInfo = {
+  type: 'variable';
+  label: string;
+  name: string;
+  id: number;
+};
+type HeaderInfo = {
+  type: 'header';
+  label: string;
+  id: number;
+};
+
+type VariableOrHeader = VariableInfo | HeaderInfo;
+
+// TODO this list should be computed from the variables actually defined by the application
+const allVariables: VariableOrHeader[] = [
+  'Common colours',
+  '--ag-foreground-color',
+  '--ag-background-color',
+  '--ag-accent-color',
+  'All colours',
+  '--ag-accent-color',
+  '--ag-advanced-filter-builder-indent-size',
+  '--ag-advanced-filter-column-pill-color',
+  '--ag-advanced-filter-join-pill-color',
+  '--ag-advanced-filter-option-pill-color',
+  '--ag-advanced-filter-value-pill-color',
+  '--ag-background-color',
+  '--ag-border-color',
+  '--ag-border-radius',
+  '--ag-borders',
+  '--ag-borders-critical',
+  '--ag-borders-input',
+  '--ag-borders-input-invalid',
+  '--ag-borders-secondary',
+  '--ag-borders-side-button',
+  '--ag-card-radius',
+  '--ag-card-shadow',
+  '--ag-cell-horizontal-border',
+  '--ag-cell-horizontal-padding',
+  '--ag-cell-widget-spacing',
+  '--ag-checkbox-background-color',
+  '--ag-checkbox-border-radius',
+  '--ag-checkbox-checked-color',
+  '--ag-checkbox-indeterminate-color',
+  '--ag-checkbox-unchecked-color',
+  '--ag-chip-background-color',
+  '--ag-chip-border-color',
+  '--ag-column-hover-color',
+  '--ag-column-select-indent-size',
+  '--ag-control-panel-background-color',
+  '--ag-data-color',
+  '--ag-disabled-foreground-color',
+  '--ag-filter-tool-panel-group-indent',
+  '--ag-font-family',
+  '--ag-font-size',
+  '--ag-foreground-color',
+  '--ag-grid-size',
+  '--ag-header-background-color',
+  '--ag-header-cell-hover-background-color',
+  '--ag-header-cell-moving-background-color',
+  '--ag-header-column-resize-handle-color',
+  '--ag-header-column-resize-handle-display',
+  '--ag-header-column-resize-handle-height',
+  '--ag-header-column-resize-handle-width',
+  '--ag-header-column-separator-color',
+  '--ag-header-column-separator-display',
+  '--ag-header-column-separator-height',
+  '--ag-header-column-separator-width',
+  '--ag-header-foreground-color',
+  '--ag-header-height',
+  '--ag-icon-font-color',
+  '--ag-icon-font-family',
+  '--ag-icon-font-weight',
+  '--ag-icon-image-display',
+  '--ag-icon-size',
+  '--ag-input-border-color',
+  '--ag-input-border-color-invalid',
+  '--ag-input-disabled-background-color',
+  '--ag-input-disabled-border-color',
+  '--ag-input-focus-border-color',
+  '--ag-input-focus-box-shadow',
+  '--ag-invalid-color',
+  '--ag-list-item-height',
+  '--ag-menu-min-width',
+  '--ag-minichart-selected-chart-color',
+  '--ag-minichart-selected-page-color',
+  '--ag-modal-overlay-background-color',
+  '--ag-odd-row-background-color',
+  '--ag-popup-shadow',
+  '--ag-range-selection-background-color',
+  '--ag-range-selection-background-color-2',
+  '--ag-range-selection-background-color-3',
+  '--ag-range-selection-background-color-4',
+  '--ag-range-selection-border-color',
+  '--ag-range-selection-border-style',
+  '--ag-range-selection-chart-background-color',
+  '--ag-range-selection-chart-category-background-color',
+  '--ag-range-selection-highlight-color',
+  '--ag-row-border-color',
+  '--ag-row-border-style',
+  '--ag-row-border-width',
+  '--ag-row-group-indent-size',
+  '--ag-row-height',
+  '--ag-row-hover-color',
+  '--ag-secondary-border-color',
+  '--ag-secondary-foreground-color',
+  '--ag-selected-row-background-color',
+  '--ag-selected-tab-underline-color',
+  '--ag-selected-tab-underline-transition-speed',
+  '--ag-selected-tab-underline-width',
+  '--ag-set-filter-indent-size',
+  '--ag-side-bar-panel-width',
+  '--ag-side-button-selected-background-color',
+  '--ag-subheader-background-color',
+  '--ag-subheader-toolbar-background-color',
+  '--ag-tab-min-width',
+  '--ag-toggle-button-border-width',
+  '--ag-toggle-button-height',
+  '--ag-toggle-button-off-background-color',
+  '--ag-toggle-button-off-border-color',
+  '--ag-toggle-button-on-background-color',
+  '--ag-toggle-button-on-border-color',
+  '--ag-toggle-button-switch-background-color',
+  '--ag-toggle-button-switch-border-color',
+  '--ag-toggle-button-width',
+  '--ag-tooltip-background-color',
+  '--ag-value-change-delta-down-color',
+  '--ag-value-change-delta-up-color',
+  '--ag-value-change-value-highlight-background-color',
+  '--ag-widget-container-horizontal-padding',
+  '--ag-widget-container-vertical-padding',
+  '--ag-widget-horizontal-spacing',
+  '--ag-widget-vertical-spacing',
+  '--ag-wrapper-border-radius  ',
+].map(
+  (str, id): VariableOrHeader =>
+    str.startsWith('--')
+      ? { type: 'variable', label: formatVariable(str), name: str, id }
+      : { type: 'header', label: str, id },
+);
+
+const defaultVariable = allVariables.find(
+  (v) => v.type === 'variable' && v.name === '--ag-foreground-color',
+) as VariableInfo;
